95f0d3bb46ef43211ba8834865a24d4c9b06e2c2
Mauricio Payán <mauropayan2002@gmail.com>
2026-03-22 12:40:12 +0100

    Fix CSS: remove p and span from global selector

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.set_page_config(
 
 st.markdown("""
     <style>
-        p, div, span, label, input {
+        div, label, input {
             font-size: 16px !important;
         }
         .st-emotion-cache-16idsys p {
