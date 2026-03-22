0297a73f04e1210f4b37b6569ad1e2b71ff58231
Mauricio Payán <mauropayan2002@gmail.com>
2026-03-22 12:26:17 +0100

    Reduce metric label font size

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ st.markdown("""
         .streamlit-expanderHeader {
             font-size: 18px !important;
         }
-        [data-testid="stMetricLabel"] p {
+        [data-testid="stMetricLabel"] {
             font-size: 160spx !important;
         }
     </style>
